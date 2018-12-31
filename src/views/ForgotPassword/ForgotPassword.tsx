dd52fe67a22e398ebad16c12947828b6ff7e1859
Igor Mathias <igor.mathias@dextra-sw.com>
2018-12-30 23:27:02 -0200

    Added a new color into the colors styles and a dummy Lost Password page

---
+++ src/views/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class ForgotPassword extends React.Component {
+
+    render() {
+        return (
+            <div>Forgot Password</div>
+        );
+    }
+}
+
+export default ForgotPassword;
